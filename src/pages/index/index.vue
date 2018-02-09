53a48f210d1c0782b7a9aa2b286bdc602ee13de0
hzCat <haizhoucat@qq.com>
2018-02-09 11:30:23 +0800

    test

---
+++ src/pages/index/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <label for="name">name:</label><input class="name" type="text" v-model="user_name">
+    <label for="password">password:</label><input class="password" type="password" v-model="user_pwd">
+    <button @click="submit">submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  date() {
+    return {
+      user_name: "",
+      user_pwd: ""
+    };
+  },
+  methods: {
+    submit() {
+      let name = this.user_name;
+      let pwd = this.user_pwd;
+      var data = {
+        name: name,
+        pwd: pwd
+      };
+      console.log(data);
+      this.ajax.post("/test/login", data).then(res => {
+        console.log(res);
+      });
+    }
+  }
+};
+</script>
